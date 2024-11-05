ef0e8d3b5e773020fa6f2a6a68c4ad332f74dfda
Dschien15 <121154243+Dschien15@users.noreply.github.com>
2024-11-05 12:31:52 +0700

    update music and image

---
+++ music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+  {
+    name: "Tieng gio",
+    artist: "Ds Chien",
+    img: "tieng-gio",
+    src: "tieng-gio",
+  },
+
+  {
+    name: "Tieng mua",
+    artist: "Ds Chien",
+    img: "okarun",
+    src: "Tieng-mua",
+  },
+  {
+    name: "Piano",
+    artist: "Ds Chien",
+    img: "okarun",
+    src: "tieng-piano",
+  },
+  {
+    name: "Song bien",
+    artist: "Ds Chien",
+    img: "okarun",
+    src: "Tieng-song-bien",
+  },
+  {
+    name: "White nose",
+    artist: "Ds Chien",
+    img: "okarun",
+    src: "white-noise",
+  },
+  {
+    name: "Wind",
+    artist: "Ds Chien",
+    img: "okarun",
+    src: "wind-chimes",
+  },
+];
