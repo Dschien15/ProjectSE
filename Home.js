0a321dc240c8663dc47f54f628b5b76b1028cf27
Dschien15 <121154243+Dschien15@users.noreply.github.com>
2024-11-13 18:12:49 +0700

    Update login

--- Home.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector("body"),
-  nav = document.querySelector("nav"),
-  modeToggle = document.querySelector(".dark-light"),
-  searchToggle = document.querySelector(".searchToggle");
-
-// js code search box
-searchToggle.addEventListener("click", () => {
-  searchToggle.classList.toggle("active");
-});
